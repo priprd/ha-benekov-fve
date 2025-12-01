79b3079eb0d767819ad264721bb702ced00886b4
David Priplata <david@pdit.eu>
2025-12-01 20:00:37 +0100

    Added consts

---
+++ custom_components/benekov_fve/const.py
@@ -0,0 +1,8 @@
+"""Constants for benekov_fve."""
+
+from logging import Logger, getLogger
+
+LOGGER: Logger = getLogger(__package__)
+
+DOMAIN = "benekov_fve"
+ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
