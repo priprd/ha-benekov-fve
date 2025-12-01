6b717bf53eb07e26469dcd4644d5e036d8768488
David Priplata <david@pdit.eu>
2025-12-01 20:11:05 +0100

    Remove wrong

--- custom_components/benekov_fve/conts.py
+++
@@ -1,8 +0,0 @@
-"""Constants for benekov_fve."""
-
-from logging import Logger, getLogger
-
-LOGGER: Logger = getLogger(__package__)
-
-DOMAIN = "benekov_fve"
-ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
